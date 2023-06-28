0a2b211dfdd7006442f5f6b169d4355b964c7a91
paichiwo <lzerucha@gmail.com>
2023-06-28 22:23:36 +0100

    Create app skeleton - rebrickable_api(), add_box_number() and get_api_key()

--- lego_sorter.py
+++ lego_sorter.py
@@ -1,68 +1,45 @@
 import json
+import requests
+
+lego_part_information = []
 
 
-# Dictionary to store the brick numbers and their corresponding boxes
-catalog = {}
+def get_api_key():
+    """Read API key from file."""
+
+    with open("api.txt") as f:
+        key = f.read()
+    return key
 
 
-# Function to add a brick number and its box to the catalog
-def add_to_catalog(brick_number, box):
-    catalog[brick_number] = box
+def rebrickable_api(part_num):
+    """Get lego part information from rebrickable.com."""
+
+    key = get_api_key()
+
+    url = f"https://rebrickable.com/api/v3/lego/parts/{part_num}/?key={key}"
+    lego_data = requests.get(url).json()
+
+    part_number = lego_data["part_num"]
+    part_name = lego_data["name"]
+    year = (lego_data["year_from"], lego_data["year_to"])
+    part_img_url = lego_data["part_img_url"]
+
+    bricklink_id = lego_data["external_ids"]["BrickLink"][0]
+    part_url = f"https://www.bricklink.com/v2/catalog/catalogitem.page?P={bricklink_id}#T=C"
+
+    return part_number, part_name, year, part_img_url, part_url
 
 
-# Function to save the catalog to a JSON file
-def save_catalog():
-    with open('catalog.json', 'w') as file:
-        json.dump(catalog, file)
+def add_box_number(part_num, box_num):
+
+    part_info_list = [rebrickable_api(part_num), box_num]
+    return part_info_list
 
 
-# Function to load an existing catalog from a JSON file
-def load_catalog():
-    global catalog
-    try:
-        with open('catalog.json', 'r') as file:
-            catalog = json.load(file)
-    except FileNotFoundError:
-        catalog = {}
+print(add_box_number("3022", "A"))
 
 
-# Function to search for a brick number in the catalog
-def search_catalog(brick_number):
-    if brick_number in catalog:
-        return catalog[brick_number]
-    else:
-        return None
 
 
-# Load an existing catalog (if available) when the app starts
-load_catalog()
 
-# Main program loop
-while True:
-    print("LEGO Sorting App")
-    print("1. Add a brick number to the catalog")
-    print("2. Search for a brick number")
-    print("3. Save and exit")
-    choice = input("Enter your choice (1-3): ")
-
-    if choice == '1':
-        brick_number = input("Enter the brick number: ")
-        box = input("Enter the storage box: ")
-        add_to_catalog(brick_number, box)
-        print("Brick added to the catalog.")
-
-    elif choice == '2':
-        brick_number = input("Enter the brick number to search: ")
-        box = search_catalog(brick_number)
-        if box:
-            print(f"The brick with number {brick_number} is in box {box}.")
-        else:
-            print(f"The brick with number {brick_number} is not found in the catalog.")
-
-    elif choice == '3':
-        save_catalog()
-        print("Catalog saved. Exiting the program.")
-        break
-
-    else:
-        print("Invalid choice. Please try again.")
