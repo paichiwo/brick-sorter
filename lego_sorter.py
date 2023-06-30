50799aa34981985f584b10f23e3fc1abef29ad21
paichiwo <lzerucha@gmail.com>
2023-06-30 12:20:51 +0100

    Implement update_window function that works with crete_part_image function to display 100x100 image from URL with a background removed.

--- lego_sorter.py
+++ lego_sorter.py
@@ -1,6 +1,12 @@
+import io
 import json
 import os
 import sys
+import webbrowser
+from urllib.request import urlopen
+from rembg import remove
+from PIL import Image as img
+from PIL import ImageTk as imgtk
 from tkinter import *
 from tkinter import messagebox, Label, PhotoImage, Entry
 
@@ -40,12 +46,35 @@ def rebrickable_api(part_num):
     return part_img_url, part_number, part_name, part_url
 
 
+def create_part_image(link):
+    """Create tkinter image from URL, remove background and resize to 100x100."""
+    u = urlopen(link)
+    raw_data = u.read()
+    u.close()
+    # Create a PIL Image object from the raw data
+    image = img.open(io.BytesIO(raw_data))
+    # Manipulate
+    resized_image = image.resize((100, 100))
+    final_image = remove(resized_image)
+    # Create a PhotoImage object from the resized image
+    photo = imgtk.PhotoImage(final_image)
+
+    return photo
+
+
 def update_window():
-
+    """Update an application window."""
     load_catalog()
-
     search_term = search_entry.get()
+    part_img_url, part_number, part_name, part_url = rebrickable_api(search_term)
 
+    image = create_part_image(part_img_url)
+    part_drawing_label.configure(image=image, bg=colors[1])
+    part_drawing_label.image = image
+
+    part_number_label.configure(text=part_number)
+    part_name_label.configure(text=part_name)
+    bricklink_button.configure(command=lambda: webbrowser.open(part_url))
     if search_term in catalog:
         box = catalog[search_term]
         box_label.configure(text=f"BOX: {box}")
@@ -58,12 +87,14 @@ def add_to_catalog():
 
 
 def save_catalog():
+    """Save catalog to .json file."""
     with open('catalog.json', 'w') as file:
         json.dump(catalog, file)
     messagebox.showinfo("Success", "Catalog saved.")
 
 
 def load_catalog():
+    """Load catalog from .json file."""
     global catalog
     try:
         with open('catalog.json', 'r') as file:
@@ -112,17 +143,17 @@ part_drawing_image = PhotoImage(file=resource_path("images/dummy_picture.png"))
 part_drawing_label = Label(image=part_drawing_image)
 part_drawing_label.place(x=220, y=101)
 
-part_number_label = Label(text="3022", justify="center", font=("Manrope", 10, 'bold'), bg=colors[1], fg='white', height=1)
-part_number_label.place(x=121, y=209, width=300)
+part_number_label = Label(text="3022", justify="center", font=("Manrope", 11, 'bold'), bg=colors[1], fg='white', height=1)
+part_number_label.place(x=21, y=210, width=500)
 
-part_name_label = Label(text="Plate 2x2", justify="center", font=("Manrope", 10, 'bold'), bg=colors[1], fg='white', height=1)
-part_name_label.place(x=121, y=229, width=300)
+part_name_label = Label(text="Plate 2x2", justify="center", font=("Manrope", 11, 'bold'), bg=colors[1], fg='white', height=1)
+part_name_label.place(x=21, y=235, width=500)
 
-bricklink_label = Label(text="Bricklink", justify="center", font=("Manrope", 10, 'bold'), bg=colors[1], fg='white', height=1)
-bricklink_label.place(x=121, y=269, width=300)
+bricklink_button = Button(text="Bricklink", borderwidth=0, font=("Manrope", 10, 'bold', 'underline'), bg=colors[1], fg='white', activebackground=colors[1], activeforeground='white')
+bricklink_button.place(x=21, y=260, width=500)
 
-box_label = Label(text="BOX: A", justify="center", font=("Manrope ExtraBold", 11), bg=colors[1], fg='white', height=1)
-box_label.place(x=121, y=300, width=300)
+box_label = Label(text="BOX: A", justify="center", font=("Manrope ExtraBold", 13), bg=colors[1], fg=colors[0], height=1)
+box_label.place(x=121, y=290, width=300)
 
 box_entry = Entry(justify='center', width=4, font=('Manrope', 12, 'bold'),
                   bg=colors[0], border=0, fg='white')
