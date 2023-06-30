fa96a6224317f91f1e528b3f83e7dbce6f10a0c2
paichiwo <lzerucha@gmail.com>
2023-06-30 19:25:40 +0100

    Implement exceptions handling.

--- lego_sorter.py
+++ lego_sorter.py
@@ -30,23 +30,24 @@ def get_api_key():
 
 
 def rebrickable_api(part_num):
-    """Get lego part information from rebrickable.com."""
+    """Get lego part information from rebrickable.com API."""
     key = get_api_key()
     url = f'https://rebrickable.com/api/v3/lego/parts/{part_num}/?key={key}'
-    lego_data = requests.get(url).json()
+    if requests.get(url).status_code == 200:
+        lego_data = requests.get(url).json()
 
-    part_img_url = lego_data['part_img_url']
-    part_number = lego_data['part_num']
-    part_name = lego_data['name']
+        part_img_url = lego_data['part_img_url']
+        part_number = lego_data['part_num']
+        part_name = lego_data['name']
 
-    bricklink_id = lego_data['external_ids']['BrickLink'][0]
-    part_url = f'https://www.bricklink.com/v2/catalog/catalogitem.page?P={bricklink_id}#T=C'
+        bricklink_id = lego_data['external_ids']['BrickLink'][0]
+        part_url = f'https://www.bricklink.com/v2/catalog/catalogitem.page?P={bricklink_id}#T=C'
 
-    return part_img_url, part_number, part_name, part_url
+        return part_img_url, part_number, part_name, part_url
 
 
 def create_part_image(link):
-    """Create tkinter image from URL, remove background and resize to 100x100."""
+    """Create tkinter image from URL, remove background and resize to 100x100 pixels."""
     u = urlopen(link)
     raw_data = u.read()
     u.close()
@@ -65,20 +66,27 @@ def update_window():
     """Update an application window."""
     load_catalog()
     search_term = search_entry.get()
-    part_img_url, part_number, part_name, part_url = rebrickable_api(search_term)
+    if search_term:
+        try:
+            part_img_url, part_number, part_name, part_url = rebrickable_api(search_term)
 
-    image = create_part_image(part_img_url)
-    part_drawing_label.configure(image=image, bg=colors[1])
-    part_drawing_label.image = image
+            image = create_part_image(part_img_url)
+            part_drawing_label.configure(image=image)
+            part_drawing_label.image = image
 
-    part_number_label.configure(text=part_number)
-    part_name_label.configure(text=part_name)
-    bricklink_button.configure(command=lambda: webbrowser.open(part_url))
-    if search_term in catalog:
-        box = catalog[search_term]
-        box_label.configure(text=f"BOX: {box}")
+            part_number_label.configure(text=part_number)
+            part_name_label.configure(text=part_name)
+            bricklink_button.configure(command=lambda: webbrowser.open(part_url))
+            message_label.configure(text="")
+            if search_term in catalog:
+                box = catalog[search_term]
+                box_label.configure(text=f"BOX: {box}")
+            else:
+                box_label.configure(text="NOT IN COLLECTION", fg=colors[0])
+        except TypeError:
+            message_label.configure(text="Wrong part number")
     else:
-        box_label.configure(text="NOT IN COLLECTION", fg=colors[0])
+        message_label.configure(text="Please enter part number.")
 
 
 def add_to_catalog():
