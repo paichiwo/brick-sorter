d6368a1353bc3b11587ffd3a2e02602fd92379fc
paichiwo <lzerucha@gmail.com>
2023-09-24 13:43:03 +0100

    Update SQL script

--- db/inventory.sql
+++ db/inventory.sql
@@ -2,11 +2,9 @@ BEGIN TRANSACTION;
 
 CREATE TABLE Inventory (
     part_number VARCHAR(50) PRIMARY KEY,
-    parent_id VARCHAR(50),
     color VARCHAR(50),
     amount INTEGER,
     box VARCHAR(10)
 );
 
 COMMIT;
-
