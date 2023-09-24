4abfc8f1a280456e77260074faa9e22f19df120a
paichiwo <lzerucha@gmail.com>
2023-09-24 14:58:05 +0100

    Implement search frame

--- src/brick_sorter.py
+++ src/brick_sorter.py
@@ -7,6 +7,8 @@ class BrickSorter(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.logo = None
+        self.search_frame = None
         self.search = None
         self.button = None
         self.message = None
@@ -17,12 +19,22 @@ class BrickSorter(ctk.CTk):
         self.gui()
 
     def gui(self):
-        self.search = ctk.CTkEntry(self)
-        self.search.pack()
-        self.button = ctk.CTkButton(self, text="Search", command=self.search_part)
-        self.button.pack()
-        self.message = ctk.CTkLabel(self, text="")
+
+
+
+        self.search_frame = ctk.CTkFrame(self)
+        self.search_frame.pack(fill='x', padx=10, pady=10)
+        self.search_frame.columnconfigure(0, weight=1)
+
+        self.search = ctk.CTkEntry(self.search_frame)
+        self.button = ctk.CTkButton(self.search_frame, text="Search", width=20, command=self.search_part)
+        self.search.grid(row=0, column=0, columnspan=2, sticky='ew', padx=10)
+        self.button.grid(row=0, column=2, padx=10, pady=10)
+
+        self.message = ctk.CTkLabel(self, text="test")
         self.message.pack()
+
+
 
     def search_part(self):
         part_img_url, part_number, part_name, part_url = self.rebrickable_api(self.search.get())
@@ -42,10 +54,8 @@ class BrickSorter(ctk.CTk):
                 part_img_url = lego_data['part_img_url']
                 part_number = lego_data['part_num']
                 part_name = lego_data['name']
-
                 bricklink_id = lego_data['external_ids']['BrickLink'][0]
                 part_url = f'https://www.bricklink.com/v2/catalog/catalogitem.page?P={bricklink_id}#T=C'
-
                 return part_img_url, part_number, part_name, part_url
             else:
                 self.message.configure(text="API Error.")
