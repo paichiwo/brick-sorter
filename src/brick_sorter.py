6f1a70b94828d57d74ec1b731ea3ae508404c979
paichiwo <lzerucha@gmail.com>
2023-09-24 17:51:21 +0100

    Fix issues with theme bg, rename vars, create methods for all UI elements.

--- src/brick_sorter.py
+++ src/brick_sorter.py
@@ -3,32 +3,27 @@ import customtkinter as ctk
 from src.helpers import rebrickable_api
 
 
-
 class BrickSorter(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.logo = None
-
         self.frame_s = None
         self.search_s = None
         self.button_s = None
-
         self.frame_r = None
 
         self.message = None
 
         self.title("Brick Sorter v0.01")
         self.geometry("600x400")
-        self.configure(fg_color="#02182b")
+        self.configure(fg_color=("#BBBBBB", "#02182b"))
         ctk.set_default_color_theme("data/brick_sorter_theme_v0.1.json")
 
         self.logo_label()
         self.search_frame()
         self.search_result_frame()
-
-        self.message = ctk.CTkLabel(self, text="test")
-        self.message.pack()
+        self.message_label()
 
     def logo_label(self):
         self.logo = ctk.CTkLabel(self, text="BRICK SORTER", text_color="#D7263D", font=("Arial", 40, 'bold'))
@@ -40,10 +35,10 @@ class BrickSorter(ctk.CTk):
         self.frame_s.pack(fill='x', padx=10)
         self.frame_s.columnconfigure(0, weight=1)
 
-        self.search_s = ctk.CTkEntry(self.frame_s, justify="center")
-        self.button_s = ctk.CTkButton(self.frame_s, text="Search", width=20, command=self.search_part)
-        self.search_s.grid(row=0, column=0, columnspan=2, sticky='ew', padx=10)
-        self.button_s.grid(row=0, column=2, sticky='w', padx=10, pady=10)
+        self.search_s = ctk.CTkEntry(self.frame_s, justify="center", font=("Any", 20, "bold"))
+        self.button_s = ctk.CTkButton(self.frame_s, text="Search", width=18, command=self.search_part)
+        self.search_s.grid(row=0, column=0, columnspan=2, sticky="ew", padx=10)
+        self.button_s.grid(row=0, column=2, padx=10, pady=10)
 
         self.search_s.bind('<Return>', lambda event=None: self.button_s.invoke())
         self.search_s.focus()
@@ -52,15 +47,16 @@ class BrickSorter(ctk.CTk):
         self.frame_r = ctk.CTkFrame(self)
         self.frame_r.pack(fill='x', padx=10, pady=10)
 
+    def message_label(self):
+        self.message = ctk.CTkLabel(self, text="test")
+        self.message.pack()
+
     def search_part(self):
         try:
-            part_img_url, part_number, part_name, part_url = rebrickable_api(self.search.get())
+            part_img_url, part_number, part_name, part_url = rebrickable_api(self.search_s.get())
             print(part_img_url)
             print(part_number)
             print(part_name)
             print(part_url)
         except (requests.exceptions.RequestException, requests.exceptions.JSONDecodeError):
             self.message.configure(text="Connection Error.")
-
-
-
