7ef7679b300dbe5b7b3c3bb15d1ac724f755e30c
paichiwo <lzerucha@gmail.com>
2023-09-29 12:24:28 +0100

    Implement labels for search_result_frame

--- src/brick_sorter.py
+++ src/brick_sorter.py
@@ -10,17 +10,20 @@ class BrickSorter(ctk.CTk):
 
         self.logo = None
         self.frame_s = None
-        self.search_s = None
-        self.button_img = None
-        self.button_s = None
+        self.search_entry = None
+        self.search_button_img = None
+        self.search_button = None
         self.frame_sr = None
-        self.image_sr = None
-        self.image_lbl = None
+        self.part_image = None
+        self.part_image_lbl = None
+        self.part_number_lbl = None
+        self.part_name_lbl = None
+        self.part_url_lbl = None
 
         self.message = None
 
         self.title("Brick Sorter v0.01")
-        self.geometry("600x400")
+        self.geometry("400x400")
         self.configure(fg_color=("#BBBBBB", "#02182b"))
         ctk.set_default_color_theme("data/brick_sorter_theme_v0.1.json")
 
@@ -39,22 +42,36 @@ class BrickSorter(ctk.CTk):
         self.frame_s.pack(fill='x', padx=10)
         self.frame_s.columnconfigure(0, weight=1)
 
-        self.search_s = ctk.CTkEntry(self.frame_s, justify="center", font=("Any", 20, "bold"))
-        self.button_img = ctk.CTkImage(Image.open("img/SEARCH_button.png"))
-        self.button_s = ctk.CTkButton(self.frame_s, image=self.button_img, text="", width=18, command=self.search_part)
-        self.search_s.grid(row=0, column=0, columnspan=2, sticky="ew", padx=10)
-        self.button_s.grid(row=0, column=2, padx=10, pady=10)
+        self.search_entry = ctk.CTkEntry(self.frame_s, justify="center", font=("Any", 20, "bold"))
+        self.search_button_img = ctk.CTkImage(Image.open("img/SEARCH_button.png"))
+        self.search_button = ctk.CTkButton(self.frame_s, image=self.search_button_img, text="", width=18, command=self.search_part)
+        self.search_entry.grid(row=0, column=0, columnspan=2, sticky="ew", padx=10)
+        self.search_button.grid(row=0, column=2, padx=10, pady=10)
 
-        self.search_s.bind('<Return>', lambda event=None: self.button_s.invoke())
-        self.search_s.focus()
+        self.search_entry.focus()
+        self.search_entry.bind('<Return>', lambda event=None: self.search_button.invoke())
 
     def search_result_frame(self):
+        
         self.frame_sr = ctk.CTkFrame(self)
         self.frame_sr.pack(fill='x', padx=10, pady=10)
+        self.frame_sr.columnconfigure(0, weight=1)
+        self.frame_sr.columnconfigure(1, weight=1)
+        self.frame_sr.rowconfigure(0, weight=1)
+        self.frame_sr.rowconfigure(1, weight=1)
+        self.frame_sr.rowconfigure(2, weight=1)
 
-        self.image_sr = ctk.CTkImage(Image.open('img/placeholder.png'))
-        self.image_lbl = ctk.CTkLabel(self.frame_sr, text="", image=self.image_sr, width=100, height=100)
-        self.image_lbl.pack(anchor='w', padx=10)
+        self.part_image = ctk.CTkImage(Image.open('img/placeholder.png'))
+
+        self.part_image_lbl = ctk.CTkLabel(self.frame_sr, text="", image=self.part_image, width=100, height=100)
+        self.part_number_lbl = ctk.CTkLabel(self.frame_sr, text="Part number")
+        self.part_name_lbl = ctk.CTkLabel(self.frame_sr, text="Part name")
+        self.part_url_lbl = ctk.CTkLabel(self.frame_sr, text="Part url")
+
+        self.part_image_lbl.grid(row=0, column=0, rowspan=3, sticky='w', padx=10)
+        self.part_number_lbl.grid(row=0, column=1, padx=10)
+        self.part_name_lbl.grid(row=1, column=1, padx=10)
+        self.part_url_lbl.grid(row=2, column=1, padx=10)
 
     def message_label(self):
         self.message = ctk.CTkLabel(self, text="test")
@@ -62,13 +79,12 @@ class BrickSorter(ctk.CTk):
 
     def search_part(self):
         try:
-            part_img_url, part_number, part_name, part_url = rebrickable_api(self.search_s.get())
+            part_img_url, part_number, part_name, part_url = rebrickable_api(self.search_entry.get())
             image = create_part_image(part_img_url)
-            self.image_lbl.configure(image=image)
-            self.image_lbl['image'] = image
 
-            print(part_number)
-            print(part_name)
-            print(part_url)
+            self.part_image_lbl.configure(image=image)
+            self.part_number_lbl.configure(text=part_number)
+            self.part_name_lbl.configure(text=part_name)
+            self.part_url_lbl.configure(text=part_url)
         except (requests.exceptions.RequestException, requests.exceptions.JSONDecodeError):
             self.message.configure(text="Connection Error.")
