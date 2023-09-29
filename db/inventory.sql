9b7dc331d2416e5a32157235d8575cc2679855ba
paichiwo <lzerucha@gmail.com>
2023-09-29 19:54:19 +0100

    Add part_name

--- db/inventory.sql
+++ db/inventory.sql
@@ -2,6 +2,7 @@ BEGIN TRANSACTION;
 
 CREATE TABLE Inventory (
     part_number VARCHAR(50) PRIMARY KEY,
+    part_name VARCHAR(150),
     color VARCHAR(50),
     amount INTEGER,
     box VARCHAR(10)
