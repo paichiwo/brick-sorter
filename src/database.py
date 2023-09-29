4bcd812f4f25d370d1d2fdaeafcf9c8cac19fb6e
paichiwo <lzerucha@gmail.com>
2023-09-29 20:18:22 +0100

    Implement skeleton for search_part method

--- src/database.py
+++ src/database.py
@@ -18,6 +18,23 @@ class Database:
     def close_db(self):
         self.con.close()
 
+    def search_part(self, part_number):
+        self.cur.row_factory = sqlite3.Row
+        query = "SELECT * FROM Inventory WHERE part_number = ?"
+        self.cur.execute(query, (part_number,))
+
+        result = self.cur.fetchall()
+        if result:
+            print("Part found:")
+            for row in result:
+                print(f"Part number: {row['part_number']}")
+                print(f"Part name: {row['part_name']}")
+                print(f"Color: {row['color']}")
+                print(f"Amount: {row['amount']}")
+                print(f"Box: {row['box']}")
+        else:
+            print("Part not found")
+
     def insert_part(self, part_number, part_name, part_color, part_amount, part_box):
         query = "INSERT INTO Inventory (part_number, part_name, color, amount, box) VALUES (?, ?, ?, ?, ?)"
         self.cur.execute(query, (part_number, part_name, part_color, part_amount, part_box))
