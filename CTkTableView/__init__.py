b791ceac91ad4d25f33310fb5241856d8087a28e
paichiwo <lzerucha@gmail.com>
2023-09-30 13:37:55 +0100

    Implement __init__.py

---
+++ CTkTableView/__init__.py
@@ -0,0 +1,11 @@
+"""
+CustomTkinter Table
+Author: Lukasz Zerucha
+License: MIT
+This is a custom table for customtkinter.
+Homepage: https://github.com/paichiwo/CTkTableView
+"""
+
+__version__ = '0.1'
+
+from .CTkTableView import CTkTableView
