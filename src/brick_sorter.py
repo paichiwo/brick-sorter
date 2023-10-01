da8b594a2ba631a662babff7a174d943a441733e
paichiwo <lzerucha@gmail.com>
2023-10-01 14:49:52 +0100

    Refactor for more focused methods

--- src/brick_sorter.py
+++ src/brick_sorter.py
@@ -3,7 +3,7 @@ import webbrowser
 import requests
 from src.gui import Gui
 from src.database import Database
-from src.helpers import rebrickable_api, create_part_image
+from src.helpers import rebrickable_api, create_part_image, part_name_shortener
 
 
 class BrickSorter(Gui):
@@ -12,6 +12,11 @@ class BrickSorter(Gui):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.db = Database()
+
+    def update_table(self, part_number):
+        if self.db.search_part(part_number):
+            self.table.insert_rows(self.db.search_part(part_number))
+            self.table.pack(fill='x', padx=10, pady=10)
 
     def search_part(self):
         """Search part online and in the database and display results"""
@@ -26,18 +31,9 @@ class BrickSorter(Gui):
                 self.part_img_lbl.configure(image=create_part_image(part_img_url))
                 self.part_nb.set(part_number)
                 self.part_nm.set(part_name)
-                if len(part_name) > 45:
-                    text = part_name[:45].split(" ")
-                    self.part_name_lbl.configure(text=" ".join(text[:-1]))
-                else:
-                    self.part_name_lbl.configure(text=part_name)
+                self.part_name_lbl.configure(text=part_name_shortener(part_name))
                 self.part_url_btn.configure(text="Bricklink", command=lambda: webbrowser.open(part_url))
-
-                search_result = self.db.search_part(part_number)
-                if search_result:
-                    self.table.insert_rows(search_result)
-                    self.table.pack(fill='x', padx=10, pady=10)
-
+                self.update_table(part_number)
         except (requests.exceptions.RequestException, requests.exceptions.JSONDecodeError):
             self.message.configure(text="Connection Error")
         except KeyError:
@@ -53,30 +49,34 @@ class BrickSorter(Gui):
 
         if all([part_number and part_name and part_color and part_amount and part_box]):
             try:
-                if not self.db.check_existing(part_number, part_color):
-                    self.db.insert_part(part_number, part_name, part_color, part_amount, part_box)
-                    self.message.configure(text="Part inserted successfully")
-                else:
-                    self.message.configure(text=f"Part: {part_number} with color: {part_color} exists in the database")
+                self.handle_add_part(part_number, part_name, part_color, part_amount, part_box)
             except sqlite3.Error as e:
                 self.message.configure(text=f"Error inserting part: {e}")
         else:
             self.message.configure(text="Please enter all of the values")
+        self.update_table(part_number)
+
+    def handle_add_part(self, part_number, part_name, part_color, part_amount, part_box):
+        if not self.db.check_existing(part_number, part_color):
+            self.db.insert_part(part_number, part_name, part_color, part_amount, part_box)
+            self.message.configure(text="Part inserted successfully")
+        else:
+            self.message.configure(text=f"Part: {part_number} with color: {part_color} exists in the database")
 
     def delete_part(self):
         values = self.table.get_values()
         if values:
             try:
-                check_deleted = self.db.delete_part(values["part_number"], values["part_color"])
-                if check_deleted == 0:
-                    self.message.configure(text="No matching part found")
-                elif check_deleted == 1:
-                    self.message.configure(text="Part deleted successfully")
-                else:
-                    self.message.configure(text=f"Unexpected number of rows deleted: {check_deleted}")
+                self.handle_delete_part(values["part_number"], values["part_color"])
             except sqlite3.Error as e:
-                self.message.configure(text="Error deleting part")
-        search_result = self.db.search_part(values['part_number'])
-        if search_result:
-            self.table.insert_rows(search_result)
+                self.message.configure(text=f"Error deleting part: {e}")
+        self.update_table(values["part_number"])
 
+    def handle_delete_part(self, part_number, part_color):
+        check_deleted = self.db.delete_part(part_number, part_color)
+        if check_deleted == 0:
+            self.message.configure(text="No matching part found")
+        elif check_deleted == 1:
+            self.message.configure(text="Part deleted successfully")
+        else:
+            self.message.configure(text=f"Unexpected number of rows deleted: {check_deleted}")
